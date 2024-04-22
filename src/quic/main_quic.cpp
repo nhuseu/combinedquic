ace2fc042d054cc622241a9aafbdd08893892f2f
nhuseu <nhu@seu.edu.cn>
2024-04-23 00:31:45 +0800

    Correction

--- src/quic/main_quic.cpp
+++ src/quic/main_quic.cpp
@@ -1,9 +1,9 @@
 /**
- * @file main_quic.cpp (https://www.seu.edu.cn/)
- * @author 论文作者???  hwu(hwu@seu.edu.cn)
- * @brief ???
- * @version 0.1
- * @date 2023-10-12
+    @file main_quic.cpp (https://github.com/nhuseu/combinedquic/blob/main/src/quic/main_quic.cpp)
+    @author nhu(nhu@seu.edu.cn)  hwu(hwu@seu.edu.cn) hyzhao(hyzhao@seu.edu.cn) ssni(ssni@seu.edu.cn) gcheng(chengguang@seu.edu.cn)
+    @brief for encrypted QUIC video identification attacks
+    @version 0.1
+    @date 2024-4-18
  */
 
 #include <iostream>
